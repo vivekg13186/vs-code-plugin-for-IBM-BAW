0187eba424b1ae5d09dc7f6c3e4b74784a0fd24d
Vivek G <vivek13186@gmail.com>
2024-04-10 09:25:48 +0200

    update view instance

--- view_instance.js
+++ view_instance.js
@@ -1,82 +1,96 @@
-var { getENVs, getEPVs, getProcessAppSettings, getRESTServers, getTeamBindings, getProcessInstanceDetails } = require("./api");
+var {
+  getProcessInstanceDetails,
+} = require("./api");
 
 const vscode = require("vscode");
-const { getConnections, getConnection, findConnection } = require("./storage");
+const { getConnections } = require("./storage");
 
-function getUri (webview, extensionUri, pathList) {
-    return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
-}
-
- 
-
-function getSelectedText () {
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) return "";
-
-    const document = editor.document;
-    const selection = editor.selection;
-    if (!document || !selection) return "";
-    const text = document.getText(selection);
-    return text || "";
+function getUri(webview, extensionUri, pathList) {
+  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
 }
 
 
-function register (id, context,treeProvider) {
+function register(id, context, treeProvider) {
+  var plugin = vscode.commands.registerCommand(id, async function () {
+    var connectionList = await getConnections(context);
+    connectionList = connectionList.map(function (con) {
+      return con.connectionName;
+    });
+    var connectionName = await vscode.window.showQuickPick(connectionList);
 
-    var plugin = vscode.commands.registerCommand(id, async function (d ) {
-        
-        var data = getSelectedText().split("\n");
-        if (data.length < 2) {
-            vscode.window.showErrorMessage("Unexpected input.Requires 'Enviroment Name','Instance ids*'");
-            return;
-        }
-        data = data.map(i=>i.trim());
-    
-        var c = await findConnection(context,data[0]);
-        if(!c)return;
-        for(var i=1;i<data.length;i++){
-            var instanceid = data[i];
-            var input = await getProcessInstanceDetails(c.host,c.username,c.password,instanceid);
-            
-            var panel = vscode.window.createWebviewPanel('Instance Details', ` ${instanceid}-Instance Details`, vscode.ViewColumn.One, {
-                enableScripts: true,
-                enableFindWidget: true
+    var instanceid = await vscode.window.showInputBox({
+      placeHolder: "2334345",
+      title: "Enter instance id",
+    });
+    if (!instanceid) {
+      vscode.window.showErrorMessage("Enter instance id");
+      return;
+    }
+
+    var c = connectionList.filter((ci) => ci.connectionName == connectionName);
+
+    if (!c) return;
+
+    var input = await getProcessInstanceDetails(
+      c.host,
+      c.username,
+      c.password,
+      instanceid
+    );
+
+    var panel = vscode.window.createWebviewPanel(
+      "Instance Details",
+      ` ${instanceid}-Instance Details`,
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true,
+        enableFindWidget: true,
+      }
+    );
+
+    var html = generateHTML(panel, context, input);
+    panel.webview.html = html;
+    panel.webview.onDidReceiveMessage(
+      (message) => {
+        switch (message.command) {
+          case "showJSON":
+            vscode.workspace.openTextDocument({
+              content: message.text,
+              language: "json",
             });
 
-            
-            var html = generateHTML(panel, context, input);
-            panel.webview.html = html;
-            panel.webview.onDidReceiveMessage(
-                message => {
-                    switch (message.command) {
-                        case 'showJSON':
-                            vscode.workspace.openTextDocument(
-                                {
-                                    content : message.text,
-                                    language :"json"
-                                }
-                            );
-                            
-                            return;
-                    }
-                },
-                undefined,
-                context.subscriptions
-            );
+            return;
         }
-       
-    });
+      },
+      undefined,
+      context.subscriptions
+    );
+  });
 
-    context.subscriptions.push(plugin);
+  context.subscriptions.push(plugin);
 }
 
-function generateHTML (panel, context, input) {
-    var app_js = getUri(panel.webview, context.extensionUri, ["media", "entry-instance.js"]);
-    var collapse_css = getUri(panel.webview, context.extensionUri, ["media", "_plugin-vue_export-helper.css"]);
-    var app_css = getUri(panel.webview, context.extensionUri, ["media", "view_instance_main.css"]);
-    
-    var icon_css = getUri(panel.webview, context.extensionUri, ["node_modules", "@vscode/codicons", "dist", "codicon.css"]);
-    return `
+function generateHTML(panel, context, input) {
+  var app_js = getUri(panel.webview, context.extensionUri, [
+    "media",
+    "entry-instance.js",
+  ]);
+  var collapse_css = getUri(panel.webview, context.extensionUri, [
+    "media",
+    "_plugin-vue_export-helper.css",
+  ]);
+  var app_css = getUri(panel.webview, context.extensionUri, [
+    "media",
+    "view_instance_main.css",
+  ]);
+
+  var icon_css = getUri(panel.webview, context.extensionUri, [
+    "node_modules",
+    "@vscode/codicons",
+    "dist",
+    "codicon.css",
+  ]);
+  return `
     <!doctype  html>
     <html lang="">
     <head> 
@@ -99,7 +113,7 @@ function generateHTML (panel, context, input) {
             <script type="module" src="${app_js}"></script>
         </body>
         </html>
-    `
+    `;
 }
 
 exports.register = register;
